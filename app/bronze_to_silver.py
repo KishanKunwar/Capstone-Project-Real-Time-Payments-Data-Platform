9bd087067b1842444159212ced077c312ae532d4
KishanKunwar <64688573+KishanKunwar@users.noreply.github.com>
2025-11-14 09:11:36 -0500

    Update bronze_to_silver.py

--- app/bronze_to_silver.py
+++ app/bronze_to_silver.py
@@ -6,23 +6,23 @@ spark = SparkSession.builder \
     .appName("Bronze to Silver Payments") \
     .getOrCreate()
 
-# 1️⃣ Read Bronze layer
+# Read Bronze layer
 bronze_path = "/opt/app/bronze/payments/"
 bronze_df = spark.read.parquet(bronze_path)
 
-# 2️⃣ Deduplicate
+#  Deduplicate
 silver_df = bronze_df.dropDuplicates(["transition_id"])
 
-# 3️⃣ Standardize data types
+# Standardize data types
 silver_df = silver_df.withColumn("amount", col("amount").cast(DoubleType()))
 
-# 4️⃣ Enforce ISO currency & clean invalid fields
+#  Enforce ISO currency & clean invalid fields
 valid_currencies = ["USD", "EUR", "GBP", "CAD"]  # Add more as needed
 silver_df = silver_df.filter(col("currency").isin(valid_currencies))
 
-# 5️⃣ Add processing timestamp
+#  Add processing timestamp
 silver_df = silver_df.withColumn("processed_ts", current_timestamp())
 
-# 6️⃣ Write Silver layer
+#  Write Silver layer
 silver_path = "/opt/app/silver/payments/"
 silver_df.write.mode("overwrite").parquet(silver_path)
